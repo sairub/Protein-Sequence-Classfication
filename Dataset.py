885fd460446586e90dbf0068e080245e87bf70ec
Sairub Naaz <91753120+sairub@users.noreply.github.com>
2023-11-03 22:12:49 -0400

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -26,25 +26,3 @@ class SequenceDataset(torch.utils.data.Dataset):
             seq += [self.word2id['<pad>']] * (self.max_len - len(seq))
         seq = torch.tensor(seq, dtype=torch.long)
         return seq
-    
-    # def preprocess(self, text):
-    #     seq = []
-        
-    #     # Encode into IDs
-    #     for word in text[:self.max_len]:
-    #         seq.append(self.word2id.get(word, self.word2id['<unk>']))
-                
-    #     # Pad to maximal length
-    #     if len(seq) < self.max_len:
-    #         seq += [self.word2id['<pad>'] for _ in range(self.max_len - len(seq))]
-                
-    #     # Convert list into tensor
-    #     seq = torch.from_numpy(np.array(seq))
-            
-    #     # One-hot encode    
-    #     one_hot_seq = torch.nn.functional.one_hot(seq.long(), num_classes=len(self.word2id), ) 
-
-    #     # Permute channel (one-hot) dim first
-    #     one_hot_seq = one_hot_seq.permute(1,0)
-
-    #     return one_hot_seq
