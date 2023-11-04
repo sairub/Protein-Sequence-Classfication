92a196f33141a6b0977da8f88b8c1cc5ac2c2965
Sairub Naaz <91753120+sairub@users.noreply.github.com>
2023-11-03 22:13:20 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,15 +38,6 @@ class Utils:
         word2id['<pad>'] = 0
         word2id['<unk>'] = 1
         return word2id
-
-    # def build_labels(self, targets):
-    #     unique_targets = targets.unique()
-    #     fam2label = {target: i for i, target in enumerate(unique_targets, start=1)}
-    #     fam2label['<unk>'] = 0
-        
-    #     print(f"There are {len(fam2label)} labels.")
-            
-    #     return fam2label
     
     def get_amino_acid_frequencies(self, data):
         aa_counter = Counter()
@@ -55,19 +46,3 @@ class Utils:
             aa_counter.update(sequence)
             
         return pd.DataFrame({'AA': list(aa_counter.keys()), 'Frequency': list(aa_counter.values())})
-    
-    # def build_vocab(self, data):
-    # # Build the vocabulary
-    #     voc = set()
-    #     rare_AAs = {'X', 'U', 'B', 'O', 'Z'}
-    #     for sequence in data:
-    #         voc.update(sequence)
-
-    #     unique_AAs = sorted(voc - rare_AAs)
-        
-    #     # Build the mapping
-    #     word2id = {w: i for i, w in enumerate(unique_AAs, start=2)}
-    #     word2id['<pad>'] = 0
-    #     word2id['<unk>'] = 1
-        
-    #     return word2id
