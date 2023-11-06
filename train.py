c328597d15f3fe3440e98b97c270101321bebf70
sairub <sairub@umich.edu>
2023-11-06 13:45:43 -0500

    changed defaults

--- train.py
+++ train.py
@@ -56,11 +56,11 @@ def validate(val_loader, model, criterion, device):
 def main():
     parser = argparse.ArgumentParser(description="Protein Classifier")
     parser.add_argument("--data_dir", type=str, default=os.path.abspath(os.path.dirname(__file__))+"/random_split", help="Path to the data directory")
-    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training")
+    parser.add_argument("--batch_size", type=int, default=256, help="Batch size for training")
     parser.add_argument("--learning_rate", type=float, default=1e-2, help="Learning rate")
     parser.add_argument("--momentum", type=float, default=0.9, help="Momentum")
     parser.add_argument("--weight_decay", type=float, default=0.01, help="Weight Decay")
-    parser.add_argument("--num_epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("--num_epochs", type=int, default=25, help="Number of training epochs")
     parser.add_argument("--num_workers", type=int, default=0, help="Number of Workers")
 
     args = parser.parse_args()
